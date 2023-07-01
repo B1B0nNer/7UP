9d4b84ed3f9afb9dd41ad0e8fe7de2eb8e556974
Nikita <barbos.nik007@gmail.com>
2023-07-01 20:07:15 +0200

    tf main

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -70,7 +70,7 @@ const books = [{
 },
 ];
 
-import { onClickDelateBtn } from "./js/onClickDelateBtn";
+import { onClickDelateBtn } from "./onClickDelateBtn";
 
 localStorage.setItem('selected-books', JSON.stringify(books));
 
