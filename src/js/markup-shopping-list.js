b554e7b30c1a1dc48f979e890ae26e6336baa862
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-07-03 01:44:40 +0300

    change

--- src/js/markup-shopping-list.js
+++ src/js/markup-shopping-list.js
@@ -1,32 +1,66 @@
 
 const shoppingListRef = document.querySelector('.shopping-list');
+const emptyListRef = document.querySelector('.empty-list');
+const objShop = {
+  'Amazon': '<img src="../images/amazon.png" alt="logo Amazon" width="62" height="19"></img>',
+  'Apple Books':'<img src="../images/apple.png" alt="logo Amazon" width="62" height="19"></img>',
+  'Barnes and Noble': '<img src="../images/baren-nobel.png" alt="logo Barnes and Noble" width="33" height="33"></img>',
+  'Books-A-Million': '<img src="../images/books-a-million.png" alt="logo Books A Million" width="62" height="28"></img>',
+  'Bookshop': '<img src="../images/book-shop.png" alt="logo Bookshop" width="33" height="33"></img>',
+  'IndieBound': '<img src="../images/india-book.png" alt="logo Indie Bound" width="35" height="28"></img>',
+};
 
-const selectedBooks = JSON.parse(localStorage.getItem('selected-books'));
+function getImages(name) {
+  if (name in objShop) {
+    const image = objShop[name];
+    return image;
+  } else return '';
+};
+  
 
-function markupShoppingList(selectedBooks) {
-    const arrSelectedBooks = selectedBooks.map(({ _id, list_name, author, book_image, description, title, buy_links }) => {
-        return `<li class="shopplist-item">
+let arrSelectedBooks = [];
+
+for (let i = 0; i < localStorage.length; i++) {
+  let key = localStorage.key(i);
+  if (key !== 'theme') {
+    let value = JSON.parse(localStorage.getItem(key))
+  arrSelectedBooks.push(value);
+  };
+};
+
+function markupShoppingList(arrSelectedBooks) {
+  const arrCardsSelectedBooks = arrSelectedBooks.map(({ _id, bookName, author, img, description, title, shops }) => {
+    const shopsName = shops.map(({ name, url }) =>  {
+      const picture =  getImages(name);
+      return `<li class="item item-book"><a href="${url}" target="_blank" class="link link-image">${picture}
+      </a></li>`
+    }).join('\n');
+      
+    return `<li class="shopplist-item" data-idcard="${_id}">
         <button type="button" class="delate-btn" data-id="${_id}">
           <svg class="delate-svg" width="16" height="16">
-            <use href="../images/sprite.svg#icon-cart"></use>
+            <use href="../images/icon.svg#icon-trash"></use>
           </svg>
         </button>
-        <img src="${book_image}" alt="" width="100" height="142" />
+        <img src="${img}" alt="" width="100" height="142" />
         <div class="card-shopplist">
           <h2 class="text_card_title text_card_title-shopplist">${title}</h2>
-          <p class="category-shopplist">${list_name}</p>
+          <p class="category-shopplist">${bookName}</p>
           <p class="description-shopplist">${description}</p>
           <div class="wrapper-card-shopplist-footer">
             <p class="text_card_author shopplist-text_card_author">${author}</p>
-            <ul class=wrapper-shops>
-              <li><a href="${buy_links[0].url}" target="_blank"><img src="" alt="${buy_links[0].name}" /></a></li>
-              <li><a href="${buy_links[1].url}" target="_blank"><img src="" alt="${buy_links[1].name}" /></a></li>
-              <li><a href="${buy_links[2].url}" target="_blank"><img src="" alt="${buy_links[2].name}" /></a></li>
-            </ul>
+            <ul class="wrapper-shops">${shopsName}</ul>
           </div></div>
-        </li>`
-    });
-    shoppingListRef.innerHTML = arrSelectedBooks.join('');   
-}
+      </li>`
+  });
+  shoppingListRef.innerHTML = arrCardsSelectedBooks.join('');   
+};
 
-markupShoppingList(selectedBooks);
+
+
+if (arrSelectedBooks.length !== 0) {
+  emptyListRef.style.display = "none";
+  markupShoppingList(arrSelectedBooks);
+} else {
+  emptyListRef.style.display = "block";
+};
--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -1,13 +1,13 @@
 // Імпорт всіх js що стосуються Sopping-list
 
-const books = [{
+const book1 = {
     "_id": "642fd89ac8cf5ee957f122da",
-    "list_name": "Middle Grade Paperback Monthly",
-    "author": "Barbara O'Connor",
-    "book_image": "https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg",
-    "description": "дапитщушкптикш лцуаимшцуг амиуг мммммммдапитщ ушкптикшлц уаимшцугами угммммммммдапитщ ушкптикшлцуаимшцуг амиугмммммммм мммммммммммм идапи тщушкптикшлцуаимшцугами угммммммм ммммммммммммид апитщу шкптикш лцуаимшцугами угмммммммм ммммммммм мммиммммммммммммиммм мммммммммми",
+    bookName: "Middle Grade Paperback Monthly",
+    author: "Barbara O'Connor",
+   img: "https://storage.googleapis.com/du-prd/books/images/9781250144058.jpg",
+    description: "дапитщушкптикш лцуаимшцуг амиуг мммммммдапитщ ушкптикшлц уаимшцугами угммммммммдапитщ ушкптикшлцуаимшцуг амиугмммммммм мммммммммммм идапи тщушкптикшлцуаимшцугами угммммммм ммммммммммммид апитщу шкптикш лцуаимшцугами угмммммммм ммммммммм мммиммммммммммммиммм мммммммммми",
     "title": "WISH",
-    "buy_links": [
+    shops: [
         {
             "name": "Amazon",
             "url": "https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20"
@@ -19,29 +19,19 @@ const books = [{
         {
             "name": "Barnes and Noble",
             "url": "https://www.anrdoezrs.net/click-7990613-11819508?url=https%3A%2F%2Fwww.barnesandnoble.com%2Fw%2F%3Fean%3D9781250144058"
-        },
-        {
-            "name": "Books-A-Million",
-            "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fp%252FWISH%252FBarbara%252BO%252527Connor%252F9781250144058&url2=https%3A%2F%2Fwww.anrdoezrs.net%2Fclick-7990613-35140%3Furl%3Dhttps%253A%252F%252Fwww.booksamillion.com%252Fsearch%253Fquery%253DWISH%252BBarbara%252BO%252527Connor"
-        },
-        {
-            "name": "Bookshop",
-            "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fbookshop.org%2Fa%2F3546%2F9781250144058&url2=https%3A%2F%2Fbookshop.org%2Fbooks%3Faffiliate%3D3546%26keywords%3DWISH"
-        },
-        {
-            "name": "IndieBound",
-            "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781250144058%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DWISH%2BBarbara%2BO%2527Connor%26aff%3DNYT"
         }
     ],
-},
+};
+
+const book2 =
 {
     "_id": "642fd89ac8cf5ee957f122db",
-    "list_name": "Non-fiction",
-    "author": "Mort",
-    "book_image": "https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg",
-    "description": "asgrsдап итщушкптикшлц уаимшцугамиугмммммммм мммммммммммми дапитщ ушкптикшлцуаим шцугамиугммммммм мммммммммммммидапитщушкптикшлцуаимшцугамиугммммммммммммммммммммидапитщушкптикшлцуаимшцугамиугммммм мммммммммммммммиerhgrthg",
+    bookName: "Non-fiction",
+    author: "Mort",
+    img: "https://storage.googleapis.com/du-prd/books/images/9780525657743.jpg",
+    description: "asgrsдап итщушкптикшлц уаимшцугамиугмммммммм мммммммммммми дапитщ ушкптикшлцуаим шцугамиугммммммм мммммммммммммидапитщушкптикшлцуаимшцугамиугммммммммммммммммммммидапитщушкптикшлцуаимшцугамиугммммм мммммммммммммммиerhgrthg",
     "title": "BOM",
-    "buy_links": [
+    shops: [
         {
             "name": "Amazon",
             "url": "https://www.amazon.com/Wish-Barbara-OConnor/dp/1250144051?tag=NYTBSREV-20"
@@ -67,21 +57,9 @@ const books = [{
             "url": "https://du-gae-books-dot-nyt-du-prd.appspot.com/redirect?url1=https%3A%2F%2Fwww.indiebound.org%2Fbook%2F9781250144058%3Faff%3DNYT&url2=https%3A%2F%2Fwww.indiebound.org%2Fsearch%2Fbook%3Fkeys%3DWISH%2BBarbara%2BO%2527Connor%26aff%3DNYT"
         }
     ],
-},
-];
+};
 
-import { onClickDelateBtn } from "./onClickDelateBtn";
+localStorage.setItem('642fd89ac8cf5ee957f122da', JSON.stringify(book1));
+localStorage.setItem('642fd89ac8cf5ee957f122db', JSON.stringify(book2));
+localStorage.setItem('theme', 'dark');
 
-localStorage.setItem('selected-books', JSON.stringify(books));
-
-
-const delateBtnRefs = document.querySelectorAll('.delate-btn');
-console.log(delateBtnRefs);
-
-delateBtnRefs.forEach(delateBtnRef => {
-    delateBtnRef.addEventListener('click', onClickDelateBtn)
-})
-
-document.addEventListener('click', (event) => {
-    console.log(event.target)
-})
