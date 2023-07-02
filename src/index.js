25f540eb79a2ecba491b9e335fbce4b750b4b23f
Oleksandr <live.gp@gmail.com>
2023-07-02 12:45:28 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import './js/header';
+import './js/loader';
+import './js/scroll_up';
 import './js/BooksApiService';
-import './js/modal-book';
-import './js/modal-regietr';
 import './js/onClickDelateBtn';
 import './js/all-categories';
 import './js/category';
-import './js/header';
 import './js/markup-shopping-list';
 import './js/shopping-list';
 import './js/support-ukraine';
+import './js/modal-book';
+import './js/modal-regietr';
---
+++ src/js/loader.js
@@ -0,0 +1,12 @@
+const loader = document.querySelector('.loader');
+
+// loader.classList.add('hidden');
+
+function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+function hideLoader() {
+  loader.classList.add('hidden');
+}
+
