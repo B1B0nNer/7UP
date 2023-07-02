576442d753d7a9bd9ec74e23da680bd11a9676c9
AlexandrOAV <a0617011995@gmail.com>
2023-07-02 16:36:25 +0300

    020723-1636

--- src/js/modal-book.js
+++ src/js/modal-book.js
@@ -1,28 +1,42 @@
-// const divEl = document.querySelector('.js-cart');
-// divEl.addEventListener('click', openModal);
+
+const witeEl = document.getElementById('wite');
+const blackEl = document.getElementById('black');
+const headEl = document.querySelector('head');
+let bookID = '';
+
+const divEl = document.querySelector('.js-cart');
+divEl.addEventListener('click', openModal);
 const bodyEl = document.querySelector('body');
 const URL = 'https://books-backend.p.goit.global/books/'
 const objShop = {
-  'Amazon': '<img src="../images/amazon.png" alt="logo Amazon" width="62" height="19"></img>',
-  'Apple Books':'<img src="../images/apple.png" alt="logo Amazon" width="62" height="19"></img>',
-  'Barnes and Noble': '<img src="../images/baren-nobel.png" alt="logo Barnes and Noble" width="33" height="33"></img>',
-  'Books-A-Million': '<img src="../images/books-a-million.png" alt="logo Books A Million" width="62" height="28"></img>',
-  'Bookshop': '<img src="../images/book-shop.png" alt="logo Bookshop" width="33" height="33"></img>',
-  'IndieBound': '<img src="../images/india-book.png" alt="logo Indie Bound" width="35" height="28"></img>',
+  'Amazon': '<img src="./img/amazon.png" alt="logo Amazon" width="62" height="19"></img>',
+  'Apple Books':'<img src="./img/apple.png" alt="logo Amazon" width="62" height="19"></img>',
+  'Barnes and Noble': '<img src="./img/baren-nobel.png" alt="logo Barnes and Noble" width="33" height="33"></img>',
+  'Books-A-Million': '<img src="./img/books-a-million.png" alt="logo Books A Million" width="62" height="28"></img>',
+  'Bookshop': '<img src="./img/book-shop.png" alt="logo Bookshop" width="33" height="33"></img>',
+  'IndieBound': '<img src="./img/india-book.png" alt="logo Indie Bound" width="35" height="28"></img>',
 };
 
 
 async function getInfoAboutBook(bookId) {
+  try {
     const response = await fetch(`${URL}${bookId}`);
+     if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
     const dataRespons = await response.json();
     const bookObj = {
-    img: dataRespons.book_image,
-    bookName: dataRespons.list_name,
-    author: dataRespons.author,
-    description: dataRespons.description,
-    shops:dataRespons.buy_links,
-  }
+      img: dataRespons.book_image,
+      bookName: dataRespons.list_name,
+      author: dataRespons.author,
+      description: dataRespons.description,
+      shops: dataRespons.buy_links,
+    }
     return bookObj;
+  }
+  catch (error) {
+    alert('Error fetching data:', error);
+  }
 };
 
 function getImeges(name) {
@@ -32,7 +46,8 @@ function getImeges(name) {
   } else return '';
 };
 async function addConten(bookId) {
-  const bookObj = await getInfoAboutBook(bookId)
+  try
+ { const bookObj = await getInfoAboutBook(bookId)
   const shopsName = bookObj.shops.map(({ name, url }) =>  {
   const pictur=  getImeges(name);
     return `<li class="item item-book"><a href="${url}" target="_blank" class="link link-image">${pictur}
@@ -49,8 +64,8 @@ let  modalHtml =  ` <div class="container-modal js-modal">
      <img src="${bookObj.img}" alt="image book ${bookObj.bookName}" width="287" height="408"></img> 
       </div>
       <div class="info-book">
-      <h2 class="name-book">${bookObj.bookName}</h2>
-     <p class="text-author">${bookObj.author}</p>
+      <h2 class="text_card_title name-book">${bookObj.bookName}</h2>
+     <p class="text_card_author text-author">${bookObj.author}</p>
      <p class="text-description">${bookObj.description}</p>
      <ul class="list shop-list">${shopsName}</ul>
      </div>
@@ -64,7 +79,10 @@ let  modalHtml =  ` <div class="container-modal js-modal">
     <p class="txt-remove">Сongratulations! You have added the book to the shopping list. To delete, press the button “Remove from the shopping list”.</p>
     </div>
   `
-  return modalHtml;
+    return modalHtml;
+  } catch {
+    alert('Error fetching data:', error);
+  }
 }
 
 function closeModalEscape(instance) {
@@ -81,13 +99,24 @@ function closeModalEscape(instance) {
       document.removeEventListener("keydown", eventHandler); 
       instance.close();
 });}
-async function openModal(event) {
-  event.preventDefault();
-  if (event.target.tagName !== 'BUTTON') {
+export async function openModal(event) {
+  if (
+    !(
+      (event.target.parentElement.nodeName === 'LI' &&
+        event.target.parentElement.dataset.bookId) ||
+      (event.target.nodeName === 'LI' && event.target.dataset.bookId)
+    )
+  ) {
     return;
   }
+
+  const bookID =
+    e.target.nodeName === 'LI'
+      ? e.target.dataset.bookId
+      : e.target.parentElement.dataset.bookId;
+  }
   
-  const instance = basicLightbox.create(await addConten('643282b1e85766588626a085'), {
+  const instance = basicLightbox.create(await addConten(bookID), {
     onShow: () => {
       document.addEventListener("keydown", closeModalEscape(instance));
       bodyEl.style.overflow = 'hidden';
@@ -102,29 +131,29 @@ async function openModal(event) {
   const btnAddEl = document.querySelector('#add');
   const btnRemoveEl = document.querySelector('#remove');
   const textElRemove = document.querySelector('.txt-remove');
-  const bookObj = await getInfoAboutBook('643282b1e85766588626a085');
+  const bookObj = await getInfoAboutBook(bookID);
   
 
-  if (localStorage.getItem(bookObj.bookName) !== null) {
+  if (localStorage.getItem(bookID) !== null) {
     btnAddEl.classList.add('hidden')
   } else {
     btnRemoveEl.classList.add('hidden')
     textElRemove.classList.add('hidden')
   }
   btnAddEl.addEventListener('click', () => {
-    addLocal(bookObj.bookName, bookObj);
+    addLocal(bookID, bookObj);
     btnAddEl.classList.add('hidden');
     btnRemoveEl.classList.remove('hidden')
     textElRemove.classList.remove('hidden')
   });
 
   btnRemoveEl.addEventListener('click', () => {
-    localStorage.removeItem(bookObj.bookName);
+    localStorage.removeItem(bookID);
     btnAddEl.classList.remove('hidden');
     btnRemoveEl.classList.add('hidden')
     textElRemove.classList.add('hidden')
   });
-}
+
 function addLocal(key, value) {
    let addElementStorage = JSON.stringify(value);
     localStorage.setItem(key, addElementStorage);
