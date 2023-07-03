2ea48b3b0c9b412ad5fcee738e4c5b5ee9cf5fdc
Nikita <barbos.nik007@gmail.com>
2023-07-03 23:03:21 +0200

    -shopping-list.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import './js/onClickDelateBtn';
 import './js/all-categories';
 import './js/category';
 import './js/markup-shopping-list';
-import './js/shopping-list';
 import './js/support-ukraine';
 import './js/modal-book';
 import './js/reg-window'
