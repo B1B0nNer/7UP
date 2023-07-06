6b46998bb5bbd7dc83306a0112baa631e94ca9e7
Nikita <barbos.nik007@gmail.com>
2023-07-06 18:57:54 +0200

    Update scroll_up.js

--- src/js/scroll_up.js
+++ src/js/scroll_up.js
@@ -1,12 +1,12 @@
 function scrollTop() {
-  const scrollTop = document.querySelector(".scrollup");
+  const scrollTopBtn = document.querySelector(".scrollup");
   if (window.scrollY >= 200)
-    scrollTop.classList.add("scrollup-show");
+    scrollTopBtn.classList.add("scrollup-show");
   else
-    scrollTop.classList.remove("scrollup-show");
+    scrollTopBtn.classList.remove("scrollup-show");
 }
 
-scrollTop.addEventListener('click', function() {
+scrollTopBtn.addEventListener('click', function() {
   window.scrollTo({
     top: 0,
     behavior: 'smooth'
