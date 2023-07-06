522e66c51e7784a33d835ce5a88ddd35257189c7
Nikita <barbos.nik007@gmail.com>
2023-07-06 16:37:51 +0200

    Update

--- src/shopList.js
+++ src/shopList.js
@@ -1,10 +1,10 @@
+import './js/firebase_sandbox';
+import './js/firebase';
 import './js/header';
 import './js/header-current-link'
 import './js/dark-theme'; 
 import './js/markup-shopping-list';
 import './js/onClickDelateBtn';
-import './js/firebase_sandbox';
-import './js/firebase';
 import './js/loader';
 import './js/scroll_up';
 import './js/BooksApiService';
